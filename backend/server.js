7bb1a5b5076ab593e0bf194f9899c55ad32d98ef
Armintas <az778@york.ac.uk>
2021-01-19 19:16:34 +0200

    Added a way to refresh token

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,8 @@ const resolvers = require('./graphql/resolvers/index.js');
 const typeDefs = require('./graphql/schema/typeDefs');
 const jsonfile = require('jsonfile');
 const authRoutes = require('./routes/authentication');
+const axios = require('axios');
+const qs = require('qs');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 8888;
@@ -40,29 +42,60 @@ passport.deserializeUser((obj, done) => {
 	done(null, obj);
 });
 
-passport.use(
-	new SpotifyStrategy(
-		{
-			clientID: process.env.SPOTIFY_CLIENT_ID,
-			clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-			callbackURL: redirect_uri
-		},
-		(accessToken, refreshToken, expires_in, profile, done) => {
-			jsonfile
-				.writeFile('token.json', {
-					accessToken: accessToken,
-					refreshToken: refreshToken,
-					expires_in: expires_in
-				})
-				.then(() => console.log('Write done'))
-				.catch(error => console.error(error));
+const spotifyStrategy = new SpotifyStrategy(
+	{
+		clientID: process.env.SPOTIFY_CLIENT_ID,
+		clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+		callbackURL: redirect_uri
+	},
+	(accessToken, refreshToken, expires_in, profile, done) => {
+		jsonfile
+			.writeFile('token.json', {
+				accessToken: accessToken,
+				refreshToken: refreshToken,
+				expires_in: expires_in
+			})
+			.then(() => console.log('Write done'))
+			.catch(error => console.error(error));
 
-			process.nextTick(() => {
-				return done(null, profile);
-			});
-		}
-	)
+		process.nextTick(() => {
+			return done(null, profile);
+		});
+	}
 );
+passport.use(spotifyStrategy);
+
+/**
+ * Refreshes token after 3500 seconds
+ */
+
+if (jsonfile.readFileSync('token.json').accessToken) {
+	console.log('here');
+	setInterval(async () => {
+		const data = jsonfile.readFileSync('token.json');
+		const response = await axios({
+			method: 'POST',
+			url: 'https://accounts.spotify.com/api/token',
+			data: qs.stringify({
+				grant_type: 'refresh_token',
+				refresh_token: data.refreshToken,
+				client_id: process.env.SPOTIFY_CLIENT_ID,
+				client_secret: process.env.SPOTIFY_CLIENT_SECRET
+			}),
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded'
+			}
+		});
+		jsonfile
+			.writeFile('token.json', {
+				accessToken: response.data.access_token,
+				refreshToken: response.data.refresh_token,
+				expires_in: response.data.expires_in
+			})
+			.then(() => console.log('Success'))
+			.catch(error => console.error(error));
+	}, 3500000);
+}
 
 /**
  * App use
