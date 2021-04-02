04f39fd083c67a92f46b57358eeadb7abdefabd4
Armintas <az778@york.ac.uk>
2021-04-02 17:55:57 +0300

    Added player from library, NOT FINISHED

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,11 +2,13 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import React, { useState } from 'react';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import SpotifyPlayer from 'react-spotify-web-playback';
 
 export const AccessToken = React.createContext('');
-
+let tokens = '';
 function MyApp({ Component, pageProps }: AppProps) {
 	const [token, setToken] = useState('');
+
 	const client = new ApolloClient({
 		uri: 'http://localhost:8888/graphql',
 		cache: new InMemoryCache()
@@ -14,6 +16,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 
 	const handleToken = (accessToken: string): void => {
 		setToken(accessToken);
+		tokens = accessToken;
 	};
 
 	return (
@@ -23,6 +26,14 @@ function MyApp({ Component, pageProps }: AppProps) {
 					{...pageProps}
 					addToken={accessToken => handleToken(accessToken)}
 				/>
+				{tokens !== '' && (
+					<div className="fixed bottom-0 w-full">
+						<SpotifyPlayer
+							token={tokens}
+							uris={['spotify:artist:6HQYnRM4OzToCYPpVBInuU']}
+						/>
+					</div>
+				)}
 			</AccessToken.Provider>
 		</ApolloProvider>
 	);
